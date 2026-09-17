c1cc869d32bb8c2f72cfc39840a26f4c5148b183
RegiMaria <regimaria015@gmail.com>
2026-09-17 16:36:06 -0300

    feat: adiciona rota de criar mensagens

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from database import get_connection
 
 app = FastAPI()
 
+
+class MessageCreate(BaseModel):
+    author: str
+    content: str
+
+
 @app.get("/")
 def read_root():
     return {"message": "Backend FastAPI funcionando!"}
+
 
 @app.get("/api/messages")
 def get_messages():
@@ -21,3 +29,24 @@ def get_messages():
         for r in rows
     ]
     return messages
+
+
+@app.post("/api/messages")
+def create_message(message: MessageCreate):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO messages (author, content) VALUES (%s, %s) RETURNING id, created_at;",
+        (message.author, message.content)
+    )
+    new_id, created_at = cur.fetchone()
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {
+        "id": new_id,
+        "author": message.author,
+        "content": message.content,
+        "created_at": str(created_at)
+    }
